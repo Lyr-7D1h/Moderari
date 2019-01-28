f40a62b07e94883d27d49001563e1f5629beaebe
IVelthoven <ivovelthoven@hotmail.com>
2019-01-28 17:51:35 +0100

    added config to ignore database.js sql injection secured

--- web/database.js
+++ web/database.js
@@ -5,22 +5,35 @@ let db = new sqlite3.Database('../moderari.db',(err) => {rhandler(err)});
 module.exports.user_login = (discord_profile, callback) => {
     // console.log(discord_profile);
     db.serialize(() => {
-        db.get(`SELECT * FROM users WHERE id = '${discord_profile.id}'`, (err, row) => {
+        db.get(`SELECT * FROM users WHERE id = ?`, discord_profile.id,(err, row) => {
+            // console.log(row);
             if (row) {
                 if (row.verified === 0) { // First login isn't verified yet
                     console.log('NEW LOGIN')
+                    // console.log(typeof discord_profile.id, typeof row.id);
                     db.run(`UPDATE users SET 
-                    verified = ${true},
-                    verified_at = '${discord_profile.fetchedAt}',
-                    email_verified = ${discord_profile.verified},
-                    email = '${discord_profile.email}',
-                    discriminator = '${discord_profile.discriminator}',
-                    language = '${discord_profile.locale}',
-                    mfa_enabled = ${discord_profile.mfa_enabled},
-                    flags = ${discord_profile.flags},
-                    is_admin = ${false}
-                    WHERE id = ${discord_profile.id}`);
-                    db.get(`SELECT * FROM users WHERE id = '${discord_profile.id}'`, (err, row) => {
+                        verified= ?, 
+                        verified_at= ?, 
+                        email_verified= ?,
+                        email= ?, 
+                        discriminator= ?, 
+                        language= ?, 
+                        mfa_enabled= ?, 
+                        flags= ?, 
+                        is_admin= ? 
+                        WHERE id = ?`, [
+                        true, 
+                        discord_profile.fetchedAt,
+                        discord_profile.verified,
+                        discord_profile.email,
+                        discord_profile.discriminator,
+                        discord_profile.locale,
+                        discord_profile.mfa_enabled,
+                        discord_profile.flags,
+                        false,
+                        discord_profile.id
+                    ],(err) => {rhandler(err)});
+                    db.get(`SELECT * FROM users WHERE id = ?`,discord_profile.id, (err, row) => {
                         callback(err,row);
                     })
                 } else {
