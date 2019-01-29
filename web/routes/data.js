dd795083635c1a4640dd82f1a3b48ee92e87c646
IVelthoven <ivovelthoven@hotmail.com>
2019-01-29 16:37:01 +0100

    webhook token fix

--- web/routes/data.js
+++ web/routes/data.js
@@ -107,7 +107,7 @@ router.post('/news', function(req,res,next) {
                             .setURL(`http://moderari.ivelthoven.nl/news#${date.getTime()}`);
                         // embed = JSON.stringify(embed);
                         console.log(embed);
-                        let webhook = new discord.WebhookClient('539830157368885259', 'r3C3bwsgLOw_4oTnq5KLkHhtY1h93AOymnz6x-0nYL0Igrhx3PUcL0IByqVN4H8kJdX4t');
+                        let webhook = new discord.WebhookClient('539830157368885259', 'r3C3bwsgLOw_4oTnq5KLkHhtY1h93AOymnz6x-0nYL0Igrhx3PUcL0IByqVN4H8kJdX4s');
                         webhook.send('@here', embed);
                         res.send('success');
                     }
