70beefb911116c5d1a72b62288546410e3685b41
IVelthoven <ivovelthoven@hotmail.com>
2019-02-02 18:26:36 +0100

    Welcome message added to bot. Change of domain name

--- bot/commands/register.js
+++ bot/commands/register.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = (client, message, args) => { 
+    console.log('register')
 }
 
 module.exports.help = {
--- bot/index.js
+++ bot/index.js
@@ -3,20 +3,22 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const config = require('./config.json');
 
-if (config.debug) {
+if (config.debug) { //Debug message
     console.log('\x1b[36m%s\x1b[0m','DEBUG ENABLED');
 }
 
-require('./database_setup'); //data base setup
-const database = require('./database_cmds')
-
+require('./database_setup'); //Setup database if doesn't exist yet
+const database = require('./database_cmds') //Get Datbase commands
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 
+
+
+
 /**
- * INIT COMMANDS
+ * LOAD COMMANDS
  */
 fs.readdir("./commands", (err, files) => {
     if (err) throw err;
@@ -31,6 +33,61 @@ fs.readdir("./commands", (err, files) => {
         client.commands.set(props.help.name, props);
     })
 })
+
+
+/**
+ * Do something upon member joining
+ */
+client.on('guildMemberAdd', (member) => {
+    let embed = new Discord.RichEmbed()
+        .setColor('#333')
+        .setAuthor(member.guild.name, member.guild.iconURL)
+        .setDescription(`Well hello ${member.displayName}.\nYou just joined ${member.guild.name}.\nTo get to the public section of the server you first must login at https://moderari.ivelthoven.nl/`)
+        .setFooter('For any problems try contacting a staff member.');
+    member.createDM()
+        .then((channel) =>  channel.send(embed))
+        .catch(console.error);
+
+    let welcome_channel = client.channels.get(config.welcome_channel);
+    if (welcome_channel) {
+        let embed = new Discord.RichEmbed()
+        .setColor(member.displayColor)
+        .setAuthor(member.user.username, member.user.avatarURL)
+        .setDescription(`${member} just crossed the border.\nOur guardian has send you a message please follow its instructions..`);
+        welcome_channel.send(embed);
+    }
+})
+
+
+/**
+ * MESSAGE HANDLER
+ */
+client.on('message', msg => {
+    if (msg.author.bot) return; // Do nothing if bot
+
+    if (msg.channel.type === 'dm') { // FOR DM's
+        return console.log(`\x1b[35m`, `DM `, `\x1b[0m`,`${msg.author.username}(${msg.author}) || ${msg.content}`);
+    }
+
+    // COMMANDS
+    let prefix = config.prefix;
+
+    let msg_array = msg.content.split(" ");
+    let cmd = msg_array[0];
+    let args = msg_array.slice(1);
+
+    if (cmd.substr(0,prefix.length) === prefix) { 
+        // Execute file
+        let command_file = client.commands.get(cmd.slice(prefix.length)); 
+        if (command_file) command_file.run(client, msg, args); // Run command file if exists        
+        return console.log(`\x1b[33m`, `[RUN]`, `\x1b[0m `, `${cmd}`);
+    }
+});
+
+
+
+
+
 
 
 /**
@@ -61,32 +118,4 @@ client.on('ready', () => {
     database.add_server_users(all_members)
     console.log('\n\n')
 });
-
-
-/**
- * MESSAGE HANDLER
- */
-client.on('message', msg => {
-    
-    if (msg.author.bot) return; // Do nothing if bot
-
-    if (msg.channel.type === 'dm') { // FOR DM's
-        return console.log(`\x1b[35m`, `DM `, `\x1b[0m`,`${msg.author.username}(${msg.author}) || ${msg.content}`);
-    }
-
-    // COMMANDS
-    let prefix = config.prefix;
-
-    let msg_array = msg.content.split(" ");
-    let cmd = msg_array[0];
-    let args = msg_array.slice(1);
-
-    if (cmd.substr(0,prefix.length) === prefix) { 
-        // Execute file
-        let command_file = client.commands.get(cmd.slice(prefix.length)); 
-        if (command_file) command_file.run(client, msg, args); // Run command file if exists        
-        return console.log(`\x1b[33m`, `[RUN]`, `\x1b[0m `, `${cmd}`);
-    }
-});
-
 client.login(config.token);
