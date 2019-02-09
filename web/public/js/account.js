4115bee50f3abc8beffb78ab172677f2adb00e58
IVelthoven <ivovelthoven@hotmail.com>
2019-02-09 11:30:33 +0100

    secure token to user page

--- web/public/js/account.js
+++ web/public/js/account.js
@@ -1,10 +1,10 @@
 let hide_all_pages = () => {
-    $('.page').each(function(i, el) {
+    $('.page').each(function (i, el) {
         $(el).css('display', 'none');
     })
 }
 let load_nav = () => {
-    $('.account_nav').each(function(i, el) {
+    $('.account_nav').each(function (i, el) {
         $(el).on('click', () => {
             hide_all_pages();
 
@@ -14,30 +14,38 @@ let load_nav = () => {
             let page = el.id.substr(8);
             // console.log(page);
             $(`#page_${page}`).show();
-            
+
         })
         // console.log(el.id);
     })
 }
-let sort_server_data = () => {
-    let data = JSON.parse($('#user_servers').html());
-    $(`#user_servers`).html('')
-    for (i in data) {
-        let server = data[i];
-        console.log(server);
-        let manageable = '';
-        if (server.is_owner) {
-            manageable += `<span class="tag manageable">Manageable</span>`;
-        }
-        console.log(manageable);
-        $('#user_servers').append(`<div id="user_server_${server.server_id}" class="user_server_block">${manageable}<span class="tag verified">Verified</span><h3><b>${server.server_name} [${server.server_id}]</b></h3></div>`);
-    }
-    // console.log(data);
+// let sort_server_data = () => {
+//     let data = JSON.parse($('#user_servers').html());
+//     $(`#user_servers`).html('')
+//     for (i in data) {
+//         let server = data[i];
+//         console.log(server);
+//         let manageable = '';
+//         if (server.is_owner) {
+//             manageable += `<span class="tag manageable">Manageable</span>`;
+//         }
+//         console.log(manageable);
+//         $('#user_servers').append(`<div id="user_server_${server.server_id}" class="user_server_block">${manageable}<span class="tag verified">Verified</span><h3><b>${server.server_name} [${server.server_id}]</b></h3></div>`);
+//     }
+//     // console.log(data);
+// }
+let secure_token_button = () => {
+    let token = $('#secure_token').html();
+    $('#secure_token').html('Click to reveal');
+    $('#secure_token').on('click', (e) => {
+        $('#secure_token_content').html(token);
+    })
 }
 $(document).ready(function () {
     hide_all_pages();
-    $(`#page_info`).show();
+    $(`#page_info`).show(); //Start with info page
     $(`#account_info`).addClass('active');
     load_nav();
-    sort_server_data();
+    // sort_server_data();
+    secure_token_button();
 });
