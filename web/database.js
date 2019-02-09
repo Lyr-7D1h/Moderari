e0a27105b8b8b1a14413c3fdf9a0738ba00c9ecc
IVelthoven <ivovelthoven@hotmail.com>
2019-02-09 18:11:05 +0100

    ip array fix

--- web/database.js
+++ web/database.js
@@ -56,19 +56,15 @@ module.exports.new_ip = (id, ip) => {
                     });
                 } else {
                     let new_ip = true;
-                    console.log(row)
-                    console.log(typeof row.ip);
                     let ip_array = JSON.parse(row.ip); // Make JSON object an array
                     for (i in row.ip) { //is array ()
                         if (row.ip[i] == ip) { //IP already in DB
                             new_ip = false;
                         }
                     }
-                    console.log(ip_array);
-                    console.log(typeof ip_array);
                     if (new_ip) { // If new IP
                         ip_array.push(ip); //Insert the newest array
-                        db.run("UPDATE users SET ip = ? WHERE id = ?", [JSON.stringify(ip_array), id], (err, row) => {
+                        db.run("UPDATE users SET ip = ? WHERE id = ?", [ip_array, id], (err, row) => {
                             rhandler(err);
                         });
                     }
